ab97a23b186e38646178e55591c33123477df8bd
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2019-02-10 18:20:36 +0900

    buy button 연결
    bookTitle, buyerAddress, bookId console test

--- web3js/js/app.js
+++ web3js/js/app.js
@@ -6,21 +6,38 @@ App = {
 
   $.getJSON('./books.json', function(data) {
       //petsRow.append(petTemplate.html());
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+      var booksRow = $('#booksRow');
+      var bookTemplate = $('#bookTemplate');
       for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].title);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].publicationyear);
-        petTemplate.find('.pet-age').text(data[i].author);
-        petTemplate.find('.pet-location').text(data[i].publisher);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-        petsRow.append(petTemplate.html());
+        bookTemplate.find('.panel-title').text(data[i].title);
+        bookTemplate.find('img').attr('src', data[i].picture);
+        bookTemplate.find('.book-year').text(data[i].publicationyear);
+        bookTemplate.find('.book-author').text(data[i].author);
+        bookTemplate.find('.book-pub').text(data[i].publisher);
+        bookTemplate.find('.btn-buy').attr('data-id', data[i].id);
+        booksRow.append(bookTemplate.html());
       }
     });
-
     //return await App.initWeb3();
-}};
+    return App.bindEvents();
+  },
+  bindEvents: function() {
+    $(document).on('click', '.btn-buy', App.handleBuy);
+  },
+
+  handleBuy: function(event){
+    event.preventDefault();
+    var bookId = parseInt($(event.target).data('id'));
+
+    $.getJSON('./books.json', function(data) {
+        var bookName = data[bookId].title;
+        var buyer = "0xF9c22321C90d0eE01Eba8fB4dBB1f7F40c061F5D";
+        console.log(bookName,buyer,bookId);
+    })
+
+  }
+
+};
 
   $(function() {
     $(window).load(function() {
