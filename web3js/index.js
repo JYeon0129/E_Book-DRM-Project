199dd1af859e1419087ac04cee1799f1a6b2bbb1
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2019-02-10 16:33:20 +0900

    webFront, solidity code 구성

--- web3js/index.js
+++ web3js/index.js
@@ -1,41 +1,108 @@
 (function() {
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    var contract = web3.eth.contract([{
-            "constant": true,
-            "inputs": [{
-                "name": "name",
-                "type": "string"
-            }],
-            "name": "get",
-            "outputs": [{
-                "name": "",
-                "type": "uint256"
-            }],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "constant": false,
-            "inputs": [{
-                    "name": "name",
-                    "type": "string"
-                },
-                {
-                    "name": "amount",
-                    "type": "uint256"
-                }
-            ],
-            "name": "set",
-            "outputs": [],
-            "payable": false,
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ]).at("0x34b6c114a8cfed2dd7db5025cc1e6069d28f9aab");
-
-    var name = "이순신";
-    var salary = contract.get(name);
-
-    console.log(name,salary.toString(10));
+    var contract = web3.eth.contract([
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "key",
+				"type": "address"
+			}
+		],
+		"name": "getBookName",
+		"outputs": [
+			{
+				"name": "bookName",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "transacs",
+		"outputs": [
+			{
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"name": "buyer",
+				"type": "string"
+			},
+			{
+				"name": "author",
+				"type": "string"
+			},
+			{
+				"name": "bookName",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "key",
+				"type": "address"
+			},
+			{
+				"name": "bookName",
+				"type": "string"
+			},
+			{
+				"name": "buyer",
+				"type": "string"
+			},
+			{
+				"name": "author",
+				"type": "string"
+			},
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "key",
+				"type": "address"
+			}
+		],
+		"name": "getBuyer",
+		"outputs": [
+			{
+				"name": "buyer",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]).at("0xba3ebc6bc43be5b158e9190ef4625f6422a09a87");
+    var key = "0xF9c22321C90d0eE01Eba8fB4dBB1f7F40c061F5D";
+    var bookName = contract.getBookName(key);
+    var buyer = contract.getBuyer(key);
+    console.log(key,bookName.toString(10),buyer.toString(10));
 })();
--- web3js/js/app.js
+++ web3js/js/app.js
@@ -3,48 +3,11 @@ App = {
   contracts: {},
 
   init: async function() {
-    // Load pets.
-      console.log("hello?");
 
-
-    /*  $.getJSON("C:\\Users\\KNK\\Documents\\GitHub\\web3js\\books.json", {
-        sessionId: "1"
-    }).done(function(data) {
-        alert("Successfully got the messages! ");
-
-        var petsRow = $('#petsRow');
-        var petTemplate = $('#petTemplate');
-
-        for (i = 0; i < data.length; i ++) {
-          petTemplate.find('.panel-title').text(data[i].title);
-          petTemplate.find('img').attr('src', data[i].picture);
-          petTemplate.find('.pet-breed').text(data[i].publicationyear);
-          petTemplate.find('.pet-age').text(data[i].author);
-          petTemplate.find('.pet-location').text(data[i].publisher);
-          petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-          console.log(data[i].authoraddr);
-          petsRow.append(petTemplate.html());
-        }
-
-        $target.empty();
-
-        var messages = [];
-
-        $.each(data,function(k,v) {
-            console.log(k + " " + JSON.stringify(v));
-        });
-
-
-        $target.append(data);
-    }).fail(function() {
-        alert("Could not reload messages!");
-    });
-*/
-
-    $.getJSON('books.json', function(data) {
+  $.getJSON('./books.json', function(data) {
+      //petsRow.append(petTemplate.html());
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
-
       for (i = 0; i < data.length; i ++) {
         petTemplate.find('.panel-title').text(data[i].title);
         petTemplate.find('img').attr('src', data[i].picture);
@@ -52,7 +15,6 @@ App = {
         petTemplate.find('.pet-age').text(data[i].author);
         petTemplate.find('.pet-location').text(data[i].publisher);
         petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-        console.log(data[i].authoraddr);
         petsRow.append(petTemplate.html());
       }
     });
