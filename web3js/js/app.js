bf2736ccf6fce3909d10ba0f495be9455b65034c
jeewoo9719 <34128041+jeewoo9719@users.noreply.github.com>
2019-02-14 20:05:15 +0900

    구매 버튼 클릭시 자동으로 set
    books.json에서 책 가격 정보 없어서 amount로 책 가격 정보 추가함
    app.js에서 handleBuy 함수 안에 web3 사용하여 blockchain이랑 연결하고 버튼 클릭시 getJSON으로 해당하는 책에 대한 정보를 읽어온 다음 블록에 새로운 트랜잭션 추가 getJSON 밖으로 빼면 set 함수가 먼저 실행되어서 안으로 넣었음

--- web3js/js/app.js
+++ web3js/js/app.js
@@ -1,9 +1,9 @@
 App = {
   web3Provider: null,
   contracts: {},
-
+  
   init: async function() {
-
+    
   $.getJSON('./books.json', function(data) {
       //petsRow.append(petTemplate.html());
       var booksRow = $('#booksRow');
@@ -17,6 +17,8 @@ App = {
         bookTemplate.find('.btn-buy').attr('data-id', data[i].id);
         booksRow.append(bookTemplate.html());
       }
+      var addres=prompt("address");
+      
     });
     //return await App.initWeb3();
     return App.bindEvents();
@@ -24,27 +26,131 @@ App = {
   bindEvents: function() {
     $(document).on('click', '.btn-buy', App.handleBuy);
   },
-
+  
   handleBuy: function(event){
     event.preventDefault();
     var bookId = parseInt($(event.target).data('id'));
-
+    var bookName,buyer,author,amount;
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+      var abi=[
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "key",
+              "type": "address"
+            },
+            {
+              "name": "bookName",
+              "type": "string"
+            },
+            {
+              "name": "buyer",
+              "type": "string"
+            },
+            {
+              "name": "author",
+              "type": "string"
+            },
+            {
+              "name": "amount",
+              "type": "uint256"
+            }
+          ],
+          "name": "set",
+          "outputs": [],
+          "payable": false,
+          "type": "function",
+          "stateMutability": "nonpayable"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "key",
+              "type": "address"
+            }
+          ],
+          "name": "getBookName",
+          "outputs": [
+            {
+              "name": "bookName",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "type": "function",
+          "stateMutability": "view"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "key",
+              "type": "address"
+            }
+          ],
+          "name": "getBuyer",
+          "outputs": [
+            {
+              "name": "buyer",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "type": "function",
+          "stateMutability": "view"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "name": "transacs",
+          "outputs": [
+            {
+              "name": "amount",
+              "type": "uint256"
+            },
+            {
+              "name": "buyer",
+              "type": "string"
+            },
+            {
+              "name": "author",
+              "type": "string"
+            },
+            {
+              "name": "bookName",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "type": "function",
+          "stateMutability": "view"
+        }
+      ];
+      web3.eth.defaultAccount=web3.eth.accounts[0];
+      var blockAddress="0xbef88e039a4866d80525d50fdfe035b2921a3ccb";
+      var contracts = web3.eth.contract(abi).at(blockAddress);
+      var key = web3.eth.accounts[1];
+      console.log(key);
     $.getJSON('./books.json', function(data) {
-        var bookName = data[bookId].title;
-        var buyer = "0xF9c22321C90d0eE01Eba8fB4dBB1f7F40c061F5D";
-        console.log(bookName,buyer,bookId);
-    })
-    $.getJSON('./bookstorage.json', function(data) {
-    
-    for(i=1;i<data.transactions.length;i++){
-     var book=data.transactions[i].record.parameters;
-     console.log(book[0]);
-     console.log(book[1]);
-     console.log(book[2]);
-     console.log(book[3]);
-     console.log(book[4]);
-    }
-  })
+      bookName = data[bookId].title;
+      buyer = "0xF9c22321C90d0eE01Eba8fB4dBB1f7F40c061F5D";
+      author=data[bookId].author;
+      amount=parseInt(data[bookId].amount);
+      console.log(bookName,buyer,bookId,amount);
+      contracts.set(key,bookName,buyer,author,amount,{from : web3.eth.defaultAccount,gas:300000});
+      console.log(key,bookName,amount);
+      var a=contracts.getBookName(key);
+      console.log(a.toString(10));
+    });
+      //contract.set(key,"a","b","c",2000,{from:key,gas:3000000});
+      //set을 할 때 어디서 나갈지 주소와 gas 지정해줘야함
   }
 };
 
